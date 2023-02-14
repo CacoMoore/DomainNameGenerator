47d39af2e7c59935f83809f8af69a6bbe94b3fbb
CacoMoore <caroavello.m@gmail.com>
2023-02-14 20:20:56 +0000

    domain name generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //console.log("hola" + ".com");
+
+  let pronoun = ["the", "our", "my"];
+  let adj = ["odd", "stinky"];
+  let noun = ["plumber", "horse"];
+  let extension = [".com", ".net", ".cl", ".io"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let e = 0; e < extension.length; e++) {
+          console.log(`${pronoun[i]}${adj[a]}${noun[n]}${extension[e]}`);
+          const para = document.createElement("p");
+          const node = document.createTextNode(
+            `${pronoun[i]}${adj[a]}${noun[n]}${extension[e]}`
+          );
+          para.appendChild(node);
+          document.body.appendChild(para);
+        }
+      }
+    }
+  }
 };
